60c37e4d5b420526a35cd9416c4e253a05779754
SimonPlazar <simon.plazar@gmail.com>
2023-07-26 00:58:54 +0200

    back to yolov8

--- segment.py
+++ segment.py
@@ -1,10 +1,11 @@
+import math
 import time
 import cv2
 import numpy as np
+from ultralytics import YOLO
 
-# Loading Mask RCNN
-net = cv2.dnn.readNetFromTensorflow("dnn\\frozen_inference_graph.pb",
-                                    "dnn\\mask_rcnn_inception_v2_coco_2018_01_28.pbtxt")
+# Load the model
+model = YOLO("dnn\\yolov8s.pt")
 
 # Create the video capture object (replace '0' with the appropriate video source)
 cap = cv2.VideoCapture(0)
@@ -21,40 +22,24 @@ while True:
     if not ret:
         break
 
-    # Detect objects
-    net.setInput(cv2.dnn.blobFromImage(frame, swapRB=True))
+    results = model.predict(source=frame, conf=0.59, classes=[0], verbose=False)[0].numpy()
 
-    boxes, masks = net.forward(["detection_out_final", "detection_masks"])
-
-    for i in range(boxes.shape[2]):
-        box = boxes[0, 0, i]
-        class_id = box[1]
-        if class_id != 0:
-            continue
-        score = box[2]
-        if score < 0.5:
-            continue
-
-        # Get box Coordinates
-        x = int(box[3] * width)
-        y = int(box[4] * height)
-        x2 = int(box[5] * width)
-        y2 = int(box[6] * height)
-
-        roi = black_image[y: y2, x: x2]
-        roi_height, roi_width, _ = roi.shape
-
-        # Get the mask
-        mask = masks[i, int(class_id)]
-        mask = cv2.resize(mask, (roi_width, roi_height))
-        _, mask = cv2.threshold(mask, 0.5, 255, cv2.THRESH_BINARY)
-
-        cv2.rectangle(frame, (x, y), (x2, y2), (255, 0, 0), 3)
-
-        # Get mask coordinates
-        contours, _ = cv2.findContours(np.array(mask, np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for cnt in contours:
-            cv2.fillPoly(roi, [cnt], (255, 0, 0))
+    # Draw each box from results directly
+    for param in results:
+        for box in param.boxes.data:
+            box_size = math.dist((box[0], box[1]), (box[2], box[3]))
+            cv2.putText(frame, "Person " + str(round(box[4], 3)),
+                        (int(box[0]), int(box[1]) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            if box_size > 180:
+                cv2.rectangle(frame, (int(box[0]), int(box[1])),
+                              (int(box[2]), int(box[3])), (0, 0, 255), 2)
+            elif box_size > 100:
+                cv2.rectangle(frame, (int(box[0]), int(box[1])),
+                              (int(box[2]), int(box[3])), (0, 165, 255), 2)
+            else:
+                cv2.rectangle(frame, (int(box[0]), int(box[1])),
+                              (int(box[2]), int(box[3])), (0, 255, 0), 2)
 
     cv2.imshow("Black Image", black_image)
     cv2.imshow('Processed Frame', frame)
