3758cec5061309e9f2f898375a3db4e6efc8c62e
SimonPlazar <simon.plazar@gmail.com>
2023-07-25 18:48:10 +0200

    randint + jepge artifacting

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from PIL import Image, ImageChops
+from PIL import Image
 import numpy as np
+import random
 
-def jpeg_artifact(image_path, quality):
+def simulate_compression_artifacts(image_path, quality):
+    distortion = ((100 - quality)/2).__floor__()
+
     # Load the image
     image = Image.open(image_path)
 
@@ -11,19 +14,33 @@ def jpeg_artifact(image_path, quality):
     # Save a copy of the original image for comparison later
     original_image = image.copy()
 
-    # Save the image with JPEG compression
+    # Get image dimensions
+    width, height = image.size
+
+    # Randomly distort pixels to simulate compression artifacts
+    for y in range(height):
+        for x in range(width):
+            r, g, b = image.getpixel((x, y))
+            noise_r = random.randint(-distortion, distortion)
+            noise_g = random.randint(-distortion, distortion)
+            noise_b = random.randint(-distortion, distortion)
+            new_r = max(0, min(255, r + noise_r))
+            new_g = max(0, min(255, g + noise_g))
+            new_b = max(0, min(255, b + noise_b))
+            image.putpixel((x, y), (new_r, new_g, new_b))
+
+    # Save the final distorted image
     image.save("compressed.jpg", format="JPEG", quality=quality)
 
-    # Load the compressed image back
     compressed_image = Image.open("compressed.jpg")
 
-    # Show the images (optional)
+    # Show the original and distorted images (optional)
     original_image.show()
     compressed_image.show()
 
 if __name__ == "__main__":
-    # Specify the image path and the desired JPEG quality (1 to 95)
+    # Specify the image path and the desired compression level (0 to 100)
     image_path = "screenshot.png"
-    jpeg_quality = 100
+    compression_level = 4  # Adjust this value for different levels of distortion
 
-    jpeg_artifact(image_path, jpeg_quality)
+    simulate_compression_artifacts(image_path, compression_level)
