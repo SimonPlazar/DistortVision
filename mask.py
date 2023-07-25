2c62ec8480910219f4aa6fd9b5c5ee6b2a3f8990
SimonPlazar <simon.plazar@gmail.com>
2023-07-25 21:07:49 +0200

    working mask file

--- mask.py
+++ mask.py
@@ -3,42 +3,41 @@ import numpy as np
 
 # Read the image
 image = cv2.imread('screenshot.png')
+image_copy = image.copy()  # make a copy of the original image
 
 # Get image shape
 h, w = image.shape[:2]
 
-# Create zero filled binary mask
-mask = np.zeros((h, w), np.uint8)
+# Create zero filled binary masks
+mask1 = np.zeros((h, w), np.uint8)
+mask2 = np.zeros((h, w), np.uint8)
 
-# Define center and radius of the circle
+# Define center and radii of the circles
 center = (w // 2, h // 2)
-radius = min(w, h) // 4
+radius1 = min(w, h) // 4
+radius2 = radius1 + 5  # make this mask slightly larger
 
-# Draw filled circle in the mask
-cv2.circle(mask, center, radius, (255), thickness=-1)
+# Draw filled circles in the masks
+cv2.circle(mask1, center, radius1, (255), thickness=-1)
+cv2.circle(mask2, center, radius2, (255), thickness=-1)
 
-# Extract the circle area from the original image
-extracted = cv2.bitwise_and(image, image, mask=mask)
+# Extract the circular area from the original image using the larger mask
+extracted = cv2.bitwise_and(image, image, mask=mask2)
 
-# At this point you can manipulate 'extracted' however you like.
+# At this point, you can manipulate 'extracted' however you like.
 # Once you're done, you can put it back to the original image.
 _, compressed_image = cv2.imencode(".jpg", extracted, [int(cv2.IMWRITE_JPEG_QUALITY), 5])
 # Decode the compressed image
 extracted = cv2.imdecode(compressed_image, cv2.IMREAD_COLOR)
 
-# Create an inverse mask
-inverse_mask = cv2.bitwise_not(mask)
-
-# Clear the circular area in the original image
-image_cleared = cv2.bitwise_and(image, image, mask=inverse_mask)
-
-# Now combine the manipulated 'extracted' with 'image_cleared'
-final_image = cv2.bitwise_or(image_cleared, extracted)
+# Replace the pixels in the original image with those from 'extracted',
+# but only for the pixels within the smaller mask.
+image_copy[np.where(mask1 == 255)] = extracted[np.where(mask1 == 255)]
 
 # Display final image
-cv2.imshow('Final Image', final_image)
+cv2.imshow('Final Image', image_copy)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Save final image
-cv2.imwrite('final_image_path.jpg', final_image)
+cv2.imwrite('final_image_path.jpg', image_copy)
