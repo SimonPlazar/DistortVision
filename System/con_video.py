f2fb41fdf8aeea0e8fcf5e3df55183021d12aeec
SimonPlazar <simon.plazar@gmail.com>
2023-08-07 16:36:12 +0200

    Messages system

--- System/con_video.py
+++ System/con_video.py
@@ -1,4 +1,5 @@
-import json
+import time
+
 import numpy as np
 import cv2
 import threading
@@ -7,7 +8,7 @@ import kafka
 import signal
 from datetime import datetime
 
-if __name__ == "__main__":
+def thread_frames():
     # Redis
     red = redis.Redis()
 
@@ -20,13 +21,68 @@ if __name__ == "__main__":
                                    group_id='grp_visualization', consumer_timeout_ms=2000)
     consumer.subscribe([topic])
 
+    # Initialize FPS calculation variables
+    start_time = time.time()
+    frame_count = 0
+
     while True:
         # Read from Redis when message is received over Kafka
         for message in consumer:
+
             if message.value.decode("utf-8") == "new_frame":
                 frame_time = datetime.fromtimestamp(message.timestamp / 1000)
                 curr_time = datetime.now()
                 diff = (curr_time - frame_time).total_seconds()
 
-                print("Time difference: {:.2f} seconds".format(diff))
+                # Exclude old frames
+                if diff < 2:
+                    frame_temp = np.frombuffer(red.get("frame:latest"), dtype=np.uint8)
 
+                    # Convert image
+                    # if (np.shape(frame_temp)[0] == 921600):
+                    # if (np.shape(frame_temp)[0] == 6220800):
+                    if (np.shape(frame_temp)[0] != 0):
+                        # frame = frame_temp.reshape((480, 640, 3))
+                        # frame = frame_temp.reshape((1080, 1920, 3))
+                        frame = frame_temp.reshape((720, 1280, 3))
+                        # frame = frame_temp.reshape((480, 640, 3))
+                        # frame = frame_temp.reshape((1080, 1920, 3))
+                        frame = frame_temp.reshape((720, 1280, 3))
+
+
+                    # Display image
+                    cv2.imshow("frame", frame)
+                    cv2.waitKey(1)
+
+                    # Calculate FPS
+                    frame_count += 1
+                    elapsed_time = time.time() - start_time
+                    fps = frame_count / elapsed_time
+                    print("FPS: " + str(fps))
+
+
+            if event.is_set():
+                break
+
+                # Stop loop
+        if event.is_set():
+            cv2.destroyAllWindows()
+            break
+
+
+def sigint_handler(signum, frame):
+    event.set()
+    thread_frm.join()
+    exit(0)
+
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+event = threading.Event()
+thread_frm = threading.Thread(target=lambda: thread_frames())
+
+if __name__ == "__main__":
+    thread_frm.start()
+    input("Press CTRL+C or Enter to stop visualization...")
+    event.set()
+    thread_frm.join()
--- System/pro_video.py
+++ System/pro_video.py
@@ -6,30 +6,42 @@ import signal
 import time
 import kafka
 
-if __name__ == "__main__":
+def thread_produce():
     # Redis
     red = redis.Redis()
 
-    # Video
-    vc = cv2.VideoCapture(0)
+    vc = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    vc.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+    # vc.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    # vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+    vc.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    fps = vc.get(cv2.CAP_PROP_FPS)
+    width = vc.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = vc.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    print(fps, width, height)
 
     # Kafka
     topic = 'frame_noticifation'
     producer = kafka.KafkaProducer(bootstrap_servers='localhost:9092')
 
     while True:
+        t_start = time.perf_counter()
         ret, frame = vc.read()
 
         # Jump back to the beginning of input
-        if not ret:
-            vc.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        # if not ret:
+        #     vc.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
         # Add frame to redis
         red.set("frame:latest", np.array(frame).tobytes())
 
         # Send notification about new frame over Kafka
-        future = producer.send(topic, b"new_frame",
-                               timestamp_ms=round(time.time() * 1000))
+        future = producer.send(topic, b"new_frame", timestamp_ms=round(time.time() * 1000))
 
         # Wait until message is delivered to Kafka
         try:
@@ -37,3 +49,32 @@ if __name__ == "__main__":
         except kafka.KafkaError:
             pass
 
+        # # Preserve FPS
+        # t_stop = time.perf_counter()
+        # t_elapsed = t_stop - t_start
+        # t_frame = 1000 / fps / 1000
+        # t_sleep = t_frame - t_elapsed
+        # if t_sleep > 0:
+        #     time.sleep(t_sleep)
+
+        # Stop loop
+        if event.is_set():
+            vc.release()
+            break
+
+
+def sigint_handler(signum, frame):
+    event.set()
+    thread.join()
+    exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
+
+event = threading.Event()
+thread = threading.Thread(target=lambda: thread_produce())
+
+if __name__ == "__main__":
+    thread.start()
+    input("Press CTRL+C or Enter to stop producing...")
+    event.set()
+    thread.join()
