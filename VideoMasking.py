a05442c26840601cae14b893fd346f98e3d0654a
SimonPlazar <simon.plazar@gmail.com>
2023-08-09 20:21:03 +0200

    Improves processing time

--- VideoMasking.py
+++ VideoMasking.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
-import time
 import torch
-import torch.nn.functional as F
 from ultralytics import YOLO
+import cvzone
+from scipy import ndimage as ndi
 
 def get_squere_mask(width, height):
     # Create a binary square mask (you can change this according to your needs)
@@ -36,7 +36,6 @@ if __name__ == "__main__":
     model = YOLO("dnn\\yolov8x-seg.pt")
 
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    # cap = cv2.VideoCapture(0)
 
     # MAGIC!!!
     cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc('m', 'j', 'p', 'g'))
@@ -49,19 +48,12 @@ if __name__ == "__main__":
     # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-    # Get the width and height of the video stream
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-    # Time and FPS variables
-    start_time_fps = time.time()
-    frame_counter = 0
-    fps = 0
+    fpsReader = cvzone.FPS()
 
     while True:
-        # Measure the processing time of the function
-        start_time = time.time()
-
         # Read a frame from the video stream
         ret, frame = cap.read()
         if not ret:
@@ -74,10 +66,12 @@ if __name__ == "__main__":
         if results is not None: # If a mask is found
             # mask = np.multiply(results.data[0].numpy(), 255).astype(np.uint8) # First mask
             mask = (results.data[0] * 255).to(torch.uint8)
+            # mask = torch.mul(results.data[0], 255).to(torch.uint8)
 
             for i in range(1, results.data.shape[0]): # For each other mask
                 #mask = np.bitwise_or(mask, np.multiply(results.data[i].numpy(), 255).astype(np.uint8)) # Combine the masks
                 mask = torch.bitwise_or(mask, (results.data[i] * 255).to(torch.uint8)) # Combine the masks
+                # mask = torch.bitwise_or(mask, torch.mul(results.data[0], 255).to(torch.uint8)) # Combine the masks
 
             # Pass the mask to cpu memory
             mask = mask.cpu().numpy()
@@ -88,34 +82,36 @@ if __name__ == "__main__":
 
             # Create dilated mask and extract the area defined by the mask from the original image
             dilated_mask = cv2.dilate(mask, None, iterations=5)
+            # dilated_mask = ndi.binary_dilation(mask, None, iterations=5)
             extracted = cv2.bitwise_and(frame, frame, mask=dilated_mask)
 
-            # Pink
-            # extracted = np.full((height, width, 3), (203,192,255), dtype=np.uint8)
-
             # Alter extracted image
-            # extracted = cv2.cvtColor(extracted, cv2.COLOR_BGR2HSV)
+
+            # extracted = np.full((height, width, 3), (203,192,255), dtype=np.uint8) # Pink
+            # extracted = cv2.cvtColor(extracted, cv2.COLOR_BGR2HSV) # Convert to HSV
             _, compressed_image = cv2.imencode(".jpg", extracted, [int(cv2.IMWRITE_JPEG_QUALITY), 6])
             extracted = cv2.imdecode(compressed_image, cv2.IMREAD_COLOR)
 
             # Replace the pixels in the original image with those from 'extracted',
-            frame[np.where(mask == 255)] = extracted[np.where(mask == 255)]
+            # frame[np.where(mask == 255)] = extracted[np.where(mask == 255)]
+
+            # Use boolean indexing to replace the pixels in the original image
+            # mask = mask == 255
+            # frame[mask] = extracted[mask]
+
+            # Use bitwise operations to replace the pixels in the original image
+            roi = cv2.bitwise_and(extracted, extracted, mask=mask)
+            inverse_mask = cv2.bitwise_not(mask)
+            background = cv2.bitwise_and(frame, frame, mask=inverse_mask)
+
+            frame = cv2.add(background, roi)
 
         else: # If no mask is found
             cv2.putText(frame, "No mask found", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2) # Display a message
 
-        # Calculate the FPS
-        frame_counter += 1
-        if (time.time() - start_time_fps) > 1:
-            fps = frame_counter / (time.time() - start_time_fps)
-            frame_counter = 0
-            start_time_fps = time.time()
-
         # Display the processed frame with FPS information
-        cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        fpsReader.update(frame, pos=(50, 80), color=(0, 255, 0), scale=5, thickness=5)
         cv2.imshow('Processed Frame', frame)
-
-        print("Time taken: {:.2f} seconds".format(time.time() - start_time))
 
         # Exit the loop when 'q' key is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
